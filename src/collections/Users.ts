8df57a124694f599d6291ec6501922b07b461975
slicycode <deparisjules180@gmail.com>
2023-12-12 12:09:02 +0100

    feature: setup email verification via resend

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,13 @@ import type { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
     slug: "users",
-    auth: true,
+    auth: {
+        verify: {
+            generateEmailHTML: ({ token }) => {
+                return `<p> hello </p>`
+            },
+        }
+    },
     access: {
         read: () => true,
         create: () => true,
