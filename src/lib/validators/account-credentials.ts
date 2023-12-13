26cbf2edb1b20841d905211d6ddb52131d0ce59f
slicycode <deparisjules180@gmail.com>
2023-12-13 14:36:01 +0100

    feature: finished proper error handling for email verification & auth

--- src/lib/validators/account-credentials.ts
+++ src/lib/validators/account-credentials.ts
@@ -1,16 +1,10 @@
-import { z } from "zod";
-
-export type TAuthCredentialsValidator = z.infer<typeof AuthCredentialsValidator>
+import { z } from 'zod'
 
 export const AuthCredentialsValidator = z.object({
-    email: z.string().email(),
-    password: z
-      .string()
-      .min(8, {
-        message: 'Password must be at least 8 characters long',
-      })
-      .max(32, {
-        message: 'Password must be at most 32 characters long',
-      }),
-  })
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters long.',
+  }),
+})
 
+export type TAuthCredentialsValidator = z.infer<typeof AuthCredentialsValidator>
