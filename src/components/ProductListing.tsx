cfe04c3a4d86ae5a9db4a5093206b5eaa2736258
slicycode <deparisjules180@gmail.com>
2023-12-14 12:27:57 +0100

    feature: add image slider to product card

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -30,6 +30,10 @@ const ProductListing = ({ product, index }: ProductLisingProps) => {
     ({ value }) => value === product.category
   )?.label
 
+  const validUrls = product.images
+    .map(({ image }) => (typeof image === 'string' ? image : image.url))
+    .filter(Boolean) as string[]
+
   if (isVisible && product) {
     return (
       <Link
@@ -39,7 +43,7 @@ const ProductListing = ({ product, index }: ProductLisingProps) => {
         })}
       >
         <div className="flex flex-col w-full">
-          <ImageSlider />
+          <ImageSlider urls={validUrls} />
 
           <h3 className="mt-4 font-medium text-sm text-gray-700">
             {product.name}
